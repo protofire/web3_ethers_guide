9f45957297c1a75ae03ad6745b64adf171d2ba77
Henry Palacios <henrydpalacios@gmail.com>
2022-08-29 23:03:51 -0300

    Switch to English

--- scripts/ethers_script.js
+++ scripts/ethers_script.js
@@ -1,29 +1,31 @@
+// Setup del provider
 const { ethers } = require("ethers");
 const rpcURL = "https://cloudflare-eth.com/";
 const provider = new ethers.providers.JsonRpcProvider(rpcURL);
-const abi = require("./ABI.json");
+
+const ERC20_ABI = require("./ABI.json");
 
 // Información de la Red
 const getLastBlock = async () => {
   const block = await provider.getBlock();
-  console.log("El Último bloque: ", block);
+  console.log("Last Block: ", block);
 };
 
 // Interacción con EOA
 const vitalikBalance = async () => {
   const balance = await provider.getBalance("vitalik.eth");
-  console.log("El Balance es:", `${ethers.utils.formatEther(balance)} ethers`);
+  console.log("Vitalik has:", `${ethers.utils.formatEther(balance)} ethers`);
 };
 
 // Leyendo del SmartContract
-const suministroDeDai = async () => {
+const getDaiTotalSupply = async () => {
   const contractAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-  const contractDai = new ethers.Contract(contractAddress, abi, provider);
+  const contractDai = new ethers.Contract(contractAddress, ERC20_ABI, provider);
 
   const suministroTotal = await contractDai.totalSupply();
 
   console.log(
-    "El suministro total será:",
+    "The DAI total supply will be:",
     suministroTotal.div(ethers.BigNumber.from(10).pow(18)).toString()
   );
 };
@@ -34,4 +36,6 @@ const suministroDeDai = async () => {
 //   "function balanceOf(address) view returns (uint)",
 // ];
 
-suministroDeDai();
+getLastBlock();
+vitalikBalance();
+getDaiTotalSupply();
--- scripts/web3_script.js
+++ scripts/web3_script.js
@@ -1,45 +1,43 @@
+// Setup
 const Web3 = require("web3");
 const rpcURL = "https://cloudflare-eth.com/";
 const web3 = new Web3(rpcURL);
-const abi = require("./ABI.json");
 
-// Información de la Red
-const ultimoBloque = () => {
+const ERC20_ABI = require("./ABI.json");
+
+// Get network info
+const getLastBlock = () => {
   web3.eth.getBlockNumber((_error, block) => {
-    console.log("Último bloque es:", block);
+    console.log("Last Block:", block);
   });
 };
 
-// Interacción con EOA
+// Interacting with EOA
 const _getVitalikAddress = async () => web3.eth.ens.getAddress("vitalik.eth");
 const vitalikBalance = async () => {
   const addressVitalik = await _getVitalikAddress();
 
   web3.eth.getBalance(addressVitalik, (_err, wei) => {
     if (_err) {
-      console.error("fallo", _err);
+      console.error("Fail", _err);
       return;
     }
 
-    console.log(
-      "La cartera de Vitalik tiene:",
-      web3.utils.fromWei(wei, "ether"),
-      "ethers"
-    );
+    console.log("Vitalik has:", web3.utils.fromWei(wei, "ether"), "ethers");
   });
 };
 
-// Leyendo del SmartContract
-const suministroDeDai = () => {
+// Reading constant from SC
+const getDaiTotalSupply = () => {
   const contractAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-  const contractDai = new web3.eth.Contract(abi, contractAddress);
+  const contractDai = new web3.eth.Contract(ERC20_ABI, contractAddress);
 
   contractDai.methods
     .totalSupply()
     .call()
     .then((result) => {
       console.log(
-        "El suministro total de DAI será:",
+        "The DAI total supply will be:",
         web3.utils
           .toBN(result)
           .div(web3.utils.BN(10).pow(web3.utils.BN(18)))
@@ -49,4 +47,6 @@ const suministroDeDai = () => {
     .catch(console.log);
 };
 
+getLastBlock();
 vitalikBalance();
+getDaiTotalSupply();
